fc7465b82fdeac56143d334c30bd4206f5ca896e
pengbo <pengbo@drplant.com.cn>
2020-04-20 18:20:42 +0800

    temp commit

--- src/main/java/ant/Valetino.java
+++ src/main/java/ant/Valetino.java
@@ -4,41 +4,41 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Valetino {
 
-	private volatile int x = 0;
+    public AtomicInteger inc = new AtomicInteger();
+    private volatile int x = 0;
 
-	public void increase() {
-		x++;
-	}
-	
-	public synchronized void  increase2() {
-		x++;
-	}
-	
-	 public  AtomicInteger inc = new AtomicInteger();
-     
-	    public  void increase3() {
-	        inc.getAndIncrement();
-	    }
+    public static void main(String[] args) {
+        // ExecutorService executor = Executors.newFixedThreadPool(2);
+        // executor.execute(command);
+        final Valetino test = new Valetino();
+        for (int i = 0; i < 100; i++) {
+            new Thread() {
+                public void run() {
+                    for (int j = 0; j < 10000; j++) {
+                        test.increase();
+                        test.increase3();
+                    }
 
-	public static void main(String args[]) {
-		// ExecutorService executor = Executors.newFixedThreadPool(2);
-		// executor.execute(command);
-		final Valetino test = new Valetino();
-		for (int i = 0; i < 100; i++) {
-			new Thread() {
-				public void run() {
-					for (int j = 0; j < 10000; j++){
-						test.increase();
-						test.increase3();
-					}
-						
-				};
-			}.start();
-		}
+                }
 
-		while (Thread.activeCount() > 1) // 保证前面的线程都执行完
-			Thread.yield();
-		System.out.println(test.x);
-		System.out.println(test.inc);
-	}
+            }.start();
+        }
+
+        while (Thread.activeCount() > 1) // 保证前面的线程都执行完
+            Thread.yield();
+        System.out.println(test.x);
+        System.out.println(test.inc);
+    }
+
+    public void increase() {
+        x++;
+    }
+
+    public synchronized void increase2() {
+        x++;
+    }
+
+    public void increase3() {
+        inc.getAndIncrement();
+    }
 }
--- src/main/java/dp/pb/test/concurrent/ForkTests.java
+++ src/main/java/dp/pb/test/concurrent/ForkTests.java
@@ -1,7 +1,6 @@
 package dp.pb.test.concurrent;
 import java.util.Random;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.*;
 
 public class ForkTests {
 
@@ -9,15 +8,26 @@ public class ForkTests {
 	
 	public static void main(String[] args) throws Exception {
 	    // 创建随机数组成的数组:
-	    long[] array = new long[400];
-	    fillRandom(array);
-	    // fork/join task:
-	    ForkJoinPool fjp = new ForkJoinPool(4); // 最大并发数4
-	    ForkJoinTask<Long> task = new SumTask(array, 0, array.length);
+//	    long[] array = new long[400];
+//	    fillRandom(array);
+//	    ThreadLocal<String> stringThreadLocal =ThreadLocal.withInitial(()->"sssss");
+//		String s = stringThreadLocal.get();
+//
+//		ExecutorService executorService = Executors.newCachedThreadPool();
+//		executorService.execute((() -> System.err.println("qqqqqq"),Integer.max(111,2222)))
+//		System.err.println(s);
+//		// fork/join task:
+//	    ForkJoinPool fjp = new ForkJoinPool(4); // 最大并发数4
+//	    ForkJoinTask<Long> task = new SumTask(array, 0, array.length);
 	    long startTime = System.currentTimeMillis();
-	    Long result = fjp.invoke(task);
-	    long endTime = System.currentTimeMillis();
-	    System.out.println("Fork/join sum: " + result + " in " + (endTime - startTime) + " ms.");
+	    System.err.println(startTime);
+	    long a =startTime>>>32;
+		System.err.println(a);
+	    long hash = startTime ^ a;
+	    System.err.println(hash);
+//	    Long result = fjp.invoke(task);
+//	    long endTime = System.currentTimeMillis();
+//	    System.out.println("Fork/join sum: " + result + " in " + (endTime - startTime) + " ms.");
 	}
 
 	private static void fillRandom(long[] array) {
@@ -29,4 +39,5 @@ public class ForkTests {
 		
 		
 	}
+
 }
